cb5771e1216b486cbb882bfb406219c5a4bdd9d3
Emil Atanasov <heitara@gmail.com>
2022-10-13 18:29:44 +0300

    SwiftUI projects were updated to reflect the changes in the lecture

---
+++ projects/SwiftUI Demo App/SwiftUI Demo App/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  SwiftUI Demo App
+//
+//  Created by Emil Atanasov on 10/10/22.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
---
+++ projects/SwiftUI Navigation/SwiftUI Navigation/SwiftUI_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NavigationApp.swift
+//  SwiftUI Navigation
+//
+//  Created by Emil Atanasov on 10/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
