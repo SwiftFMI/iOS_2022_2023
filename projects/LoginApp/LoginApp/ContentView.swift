32afc3f6fc053be716d9c2ad07df5c4b3422432a
Emil Atanasov <heitara@gmail.com>
2022-10-27 19:07:01 +0300

    Add if in the view

--- projects/LoginApp/LoginApp/ContentView.swift
+++ projects/LoginApp/LoginApp/ContentView.swift
@@ -10,30 +10,34 @@ import SwiftUI
 struct ContentView: View {
     @State var username: String = ""
     @State var password: String = ""
+    @State var isLogged = false
     var body: some View {
-        VStack {
-            
-            Text("Login")
-                .font(.largeTitle)
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            HStack {
-                Text("Username:")
-                TextField("", text: $username, prompt: Text("your@email"))
+        if isLogged {
+            VStack {
+                Text("Login")
+                    .font(.largeTitle)
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                HStack {
+                    Text("Username:")
+                    TextField("", text: $username, prompt: Text("your@email"))
+                }
+                HStack {
+                    Text("Password:")
+                    SecureField("", text: $password, prompt: Text("your passsword"))
+                        .autocorrectionDisabled(true)
+                }
+                Button {
+                    username = "Hello"
+                } label: {
+                    Text("Print")
+                }
             }
-            HStack {
-                Text("Password:")
-                SecureField("", text: $password, prompt: Text("your passsword"))
-                    .autocorrectionDisabled(true)
-            }
-            Button {
-               username = "Hello"
-            } label: {
-               Text("Print")
-            }
+            .padding()
+        } else {
+            Text("Noooo!")
         }
-        .padding()
     }
 }
 
