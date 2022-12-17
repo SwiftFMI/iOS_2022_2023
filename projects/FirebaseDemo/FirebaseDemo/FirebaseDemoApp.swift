2b8640ae4ebf7713fa7c23baaf3e9c90e891fff4
Emil Atanasov <heitara@gmail.com>
2022-12-17 18:20:57 +0200

    Първа версия на финалните проекти

---
+++ projects/FirebaseDemo/FirebaseDemo/FirebaseDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseDemoApp.swift
+//  FirebaseDemo
+//
+//  Created by Emil Atanasov on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
