52d01804537bb7d62b188135a3a9bbf316177a85
Emil Atanasov <heitara@gmail.com>
2023-01-02 00:21:24 +0200

    Дабавени са допълнителни записки за Xcode. Отстранени са част от правописните грешки

--- projects/SwiftUI Demo App/SwiftUI Demo App/ContentView.swift
+++ projects/SwiftUI Demo App/SwiftUI Demo App/ContentView.swift
@@ -26,6 +26,7 @@ struct ContentView: View {
             Button("Test button") {
                 print("test")
             }
+            CardView()
 
         }
         .padding()
---
+++ projects/SwiftUI Demo App/SwiftUI Demo App/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  SwiftUI Demo App
+//
+//  Created by Emil Atanasov on 1.01.23.
+//
+
+import Foundation
+
+
+func testLibrary() {
+    // nothing
+}
